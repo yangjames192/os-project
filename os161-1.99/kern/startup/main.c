8637080bf66e3991dec9584c540fe4f954a5b506
yangjames192 <yangjames1992@gmail.com>
2014-12-15 09:42:24 -0500

    change name to yusong yang

--- os161-1.99/kern/startup/main.c
+++ os161-1.99/kern/startup/main.c
@@ -101,7 +101,7 @@ boot(void)
 	kprintf("%s", harvard_copyright);
 	kprintf("\n");
 
-	kprintf("james yang %s (%s #%d)\n", 
+	kprintf("yusong yang %s (%s #%d)\n", 
 		GROUP_VERSION, buildconfig, buildversion);
 	kprintf("\n");
 
